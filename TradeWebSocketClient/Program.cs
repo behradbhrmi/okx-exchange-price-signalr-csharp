09497f0dc910b4155e724465c9bdba6ba414a6c4
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-05-05 21:38:14 +0330

    initialize

---
+++ TradeWebSocketClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Net.WebSockets;
+
+
+internal class Program
+{
+    private const string API_URL = "wss://streamer.cryptocompare.com/v2?api_key=e88a272ec4b86493dfa7d161e3c06090fe66ad22035e10e4b2b82c9a3ea68dd1";
+
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var cancellationToken = new CancellationTokenSource();
+        System.Net.WebSockets.ClientWebSocket websocket = new
+            System.Net.WebSockets.ClientWebSocket();
+
+        websocket.ConnectAsync(new Uri(API_URL), cancellationToken.Token).Wait();
+
+        Console.WriteLine("Connected");
+
+        byte[] buffer = new byte[1024];
+
+
+
+        while (true) 
+        {
+
+            if (websocket.State == System.Net.WebSockets.WebSocketState.Open)
+            {
+                var result = await websocket.ReceiveAsync(buffer, cancellationToken.Token);
+                Console.WriteLine("Data received");
+                var resultText = System.Text.Encoding.UTF8.GetString(buffer);
+                await Console.Out.WriteLineAsync(resultText);
+            }
+
+            if (cancellationToken.IsCancellationRequested) break;
+
+
+        }
+    }
+}
---
+++ csSignalR/Program.cs
@@ -0,0 +1,48 @@
+using SignalRChat.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace csSignalR
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddSignalR();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+            app.MapHub<ChatHub>("/chatHub");
+
+            app.Run();
+        }
+    }
+}
+
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
