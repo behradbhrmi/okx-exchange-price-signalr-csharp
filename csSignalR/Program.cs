c80939862c93546c3a7cdf261d1d6c2b3b46c4bc
Behrad Bahrami <behrad.bhrmi@gmail.com>
2023-05-25 23:10:29 +0330

    fix

--- csSignalR/Program.cs
+++ csSignalR/Program.cs
@@ -16,16 +16,9 @@ namespace csSignalR
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-            }
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.MapRazorPages();
             app.MapHub<ChatHub>("/chatHub");
